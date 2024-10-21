0013f04754563d2fd9f9559e0009cf1449f357be
Marie Idleman <marie.idleman@posit.co>
2024-10-21 10:44:45 -0500

    cleanup

--- tests/electron.spec.ts
+++ tests/electron.spec.ts
@@ -1,9 +1,7 @@
 import { _electron, test, expect } from "@playwright/test";
 
-test("should toggle checkbox", async ({ trace }) => {
+test("basic electron test", async ({ trace }) => {
   const electronApp = await _electron.launch({ args: ["main.js"] });
-
-  // Get the first window that the app opens, wait if necessary.
   const window = await electronApp.firstWindow();
 
   await window.goto("https://playwright.dev/");
@@ -11,6 +9,5 @@ test("should toggle checkbox", async ({ trace }) => {
   await window.click("text=Get Started");
   await expect(window).toHaveTitle(/Installation/);
 
-  // Exit app.
   await electronApp.close();
 });
