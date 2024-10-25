0fbdaf53fdf187c6bf187e5d58b4ee74fbb4acfc
Marie Idleman <marie.idleman@posit.co>
2024-10-25 12:24:55 -0500

    tracing example

--- tests/electron.spec.ts
+++ tests/electron.spec.ts
@@ -1,16 +1,30 @@
 import { _electron, test, expect } from "@playwright/test";
 
-// Run test with "Debug Test" play button - test runs w/o issue
-// Run test with "Run Test" play button - test fails - unable to launch Electron
-
 test("basic electron test", async () => {
-  const electronApp = await _electron.launch({ args: ["main.js"] });
+  // launch the app
+  const electronApp = await _electron.launch({
+    args: ["main.js"],
+  });
   const window = await electronApp.firstWindow();
 
+  // start tracing
+  await window
+    .context()
+    .tracing.start({ name: "ETracing", screenshots: true, snapshots: true });
+
+  // do test
   await window.goto("https://playwright.dev/");
   await expect(window).toHaveTitle(/Playwright/);
   await window.click("text=Get Started");
   await expect(window).toHaveTitle(/Installation/);
 
+  // stop tracing
+  const path = test.info().outputPath("electron-trace.zip");
+  await window.context().tracing.stop({ path });
+  test
+    .info()
+    .attachments.push({ name: "trace", path, contentType: "application/zip" });
+
+  // close the app
   await electronApp.close();
 });
